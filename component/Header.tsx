f2c55c43c206418c6ead2ec264797ef5635e11fa
Tehseen Afzal <abuashja@gmail.com>
2022-10-18 21:52:31 +0500

    Create Header.tsx

---
+++ component/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Box
+      as="section"
+      bg="#6B46C1"
+      color="#F7FAFC"
+      textAlign="center"
+      pt="88.45px"
+      pb="198px"
+      px="23px"
+    >
+      <Heading fontWeight="800" fontSize="48px">
+        Simple pricing for your business
+      </Heading>
+      <Text fontWeight="500" fontSize="24px" pt="16.08px">
+        Plans that are carefully crafted to suit your business
+      </Text>
+    </Box>
+  );
+};
+
+export default Header;

43d2e18ea4122e20d5e4f3619bf74584354509ea
Tehseen Afzal <abuashja@gmail.com>
2022-10-21 00:20:23 +0500

    Revert "Update index.tsx"
    This reverts commit df920aaa37294b3b092e091561ce66f89da3aa4f.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 import Header from "../component/Header";
 import Price from "../component/Price";
 
